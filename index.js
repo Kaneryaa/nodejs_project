19c4c43524a3ffaadd183ba996b6435cf5f09773
amrutavichare <amruta.vichare@boppotechnologies.com>
2022-06-27 12:10:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config({ path: __dirname + '/.env' });
+const express = require('express');
+const mongoose = require('mongoose');
+var cors = require('cors');
+var app = express();
+app.use(cors());
+
+//Routes
+app.get('/auth', function (req, res) {
+  res.send('hello world');
+});
+
+app.use(express.json({ extended: false }));
+app.listen(3001, function () {
+  console.log('Server is Up');
+});
+
+//Routes ends
+
+let mongoDB = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB, {useNewUrlParser: true});
+
+mongoose.connection.once("open",function(){
+  console.log("Database connected Successfully");
+}).on("error",function(err){
+  console.log("Error", err);
+})
