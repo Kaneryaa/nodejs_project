2d6865dc5b6e90e5950efc47483bc1a48d8151fa
mayur1106 <mayur@roadzen.io>
2022-07-05 15:16:33 +0530

    fix: branch validation fixes

--- .validate-branch-namerc.js
+++ .validate-branch-namerc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pattern: "^(feature|fix|hotfix|release)-.+$",
+  pattern: "^(feature|fix|hotfix|release)-^(main|staging|production)-.+$",
   errorMsg:
     "🤨 You fool! The branch you're trying to push does not Honor The Code.",
 };
